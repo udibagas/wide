288808d19d4fce355d5556df9d3ba9167feb3adb
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-04-05 16:18:54 +0700

    update harga wide4

--- app/Console/Commands/GenerateScript.php
+++ app/Console/Commands/GenerateScript.php
@@ -74,8 +74,10 @@ class GenerateScript extends Command
                 'profiles' => [
                     '1jam' => ['uptime' => '1h', 'price' => 10000, 'seller_price' => 8000, 'validity' => '1hari'],
                     '2jam' => ['uptime' => '2h', 'price' => 15000, 'seller_price' => 12000, 'validity' => '1hari'],
-                    '3jam' => ['uptime' => '3h', 'price' => 20000, 'seller_price' => 15000, 'validity' => '1hari'],
                     '5jam' => ['uptime' => '5h', 'price' => 25000, 'seller_price' => 20000, 'validity' => '2hari'],
+                    'Wan-1jam' => ['uptime' => '1h', 'price' => 10000, 'seller_price' => 7000, 'validity' => '1hari'],
+                    'Wan-2jam' => ['uptime' => '2h', 'price' => 15000, 'seller_price' => 11000, 'validity' => '1hari'],
+                    'Wan-5jam' => ['uptime' => '5h', 'price' => 25000, 'seller_price' => 19000, 'validity' => '2hari'],
                 ]
             ],
 
