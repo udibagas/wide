800b38f2ef8523aba386bf7f589a4b63ca2e1f65
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-06-12 14:12:25 +0700

    rename file

--- app/Console/Commands/MakeApi.php
+++ app/Console/Commands/MakeApi.php
@@ -5,21 +5,21 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 
-class MakeMaster extends Command
+class MakeApi extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'make:master {name}';
+    protected $signature = 'make:api {name}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Make master';
+    protected $description = 'Make CRUD API';
 
     /**
      * Create a new command instance.
